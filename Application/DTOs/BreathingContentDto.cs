6f4879dbe8feaf30aa0d289bd6df76b602de6196
mlhonbasi <melihonbasi16@gmail.com>
2025-05-08 02:14:56 +0300

    update BreathingContent controller and service

---
+++ Application/DTOs/BreathingContentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class BreathingContentDto
+    {
+        public Guid ContentId { get; set; }
+        public int StepCount { get; set; }
+        public int Duration { get; set; }
+        public string Steps { get; set; }
+    }
+}
--- Application/Services/Contents/BreathingContents/IBreathingContentService.cs
+++ Application/Services/Contents/BreathingContents/IBreathingContentService.cs
@@ -1,9 +1,13 @@
-﻿using Application.Services.Contents.BreathingContents.Models;
+﻿using Application.DTOs;
+using Application.Services.Contents.BreathingContents.Models;
 
 namespace Application.Services.Contents.BreathingContents
 {
     public interface IBreathingContentService
     {
         Task CreateBreathingContentAsync(CreateBreathingContentRequest request);
+        Task<BreathingContentDto> GetByContentIdAsync(Guid contentId);
+        Task UpdateAsync(Guid contentId, UpdateBreathingContentRequest request);
+        Task DeleteAsync(Guid contentId);
     }
 }
