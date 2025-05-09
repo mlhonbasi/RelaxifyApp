8e37993d2627b7221129d5419adc33875834b1c6
mlhonbasi <melihonbasi16@gmail.com>
2025-05-09 16:00:46 +0300

    breathing content service

--- Application/Services/Contents/BreathingContents/BreathingContentService.cs
+++ Application/Services/Contents/BreathingContents/BreathingContentService.cs
@@ -12,6 +12,9 @@ namespace Application.Services.Contents.BreathingContents
     {
         public async Task CreateBreathingContentAsync(CreateBreathingContentRequest request)
         {
+            int deneme = 0;
+            int ab = 0;
+            int c = 0;
             var contentId = await contentService.CreateContentAsync(request.ContentRequest); //Create the main content first then get the ID
 
             var breathingContent = new BreathingContent
