03751c8ee123321eff0337f649d3e9a919287f6f
mlhonbasi <melihonbasi16@gmail.com>
2025-05-22 03:14:05 +0300

    added stress test result endpoint

--- Domain/Interfaces/IStressTestResultRepository.cs
+++ Domain/Interfaces/IStressTestResultRepository.cs
@@ -5,5 +5,6 @@ namespace Domain.Interfaces
     public interface IStressTestResultRepository : IRepository<StressTestResult>
     {
         Task<IList<StressTestResult>> GetUserResultsAsync(Guid userId);
+        Task<StressTestResult?> GetLastUserTestResult(Guid userId);
     }
 }
--- Infrastructure/Repositories/StressTestResultRepository.cs
+++ Infrastructure/Repositories/StressTestResultRepository.cs
@@ -7,6 +7,14 @@ namespace Infrastructure.Repositories
 {
     public class StressTestResultRepository(RelaxifyDbContext context) : GenericRepository<StressTestResult>(context), IStressTestResultRepository
     {
+        public async Task<StressTestResult?> GetLastUserTestResult(Guid userId)
+        {
+            return await context.StressTestResults
+                .Where(r => r.UserId == userId)
+                .OrderByDescending(r => r.CreatedAt)
+                .FirstOrDefaultAsync();
+        }
+
         public async Task<IList<StressTestResult>> GetUserResultsAsync(Guid userId)
         {
             return await context.StressTestResults
